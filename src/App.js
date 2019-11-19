76187df8f4e0dc3ffe8897168f774cc1e5c0f6ba
Reid Hanson <reidhans@buffalo.edu>
2019-11-19 18:03:33 -0500

    colors changing no color numbers or functioning random button

--- src/App.js
+++ src/App.js
@@ -2,28 +2,35 @@ import React, { useState } from 'react'
 // import Format from './components/setup'
 
 const App = () => {
-  const [hue, setHue] = useState(0)
-  const [saturation, setSaturation] = useState(0)
-  const [lightness, setLightness] = useState(0)
+  const [hue, setHue] = useState(360)
+  const [saturation, setSaturation] = useState(3)
+  const [lightness, setLightness] = useState(5)
 
   const changeHue = eventData => {
-    if (hue >= 0) {
+    if (eventData) {
       setHue(hue + 1)
     }
   }
+
   const changeSaturation = eventData => {
-    if (saturation >= 0) {
+    if (eventData) {
       setSaturation(saturation + 1)
     }
   }
   const changeLightness = eventData => {
-    if (lightness >= 0) {
+    if (eventData) {
       setLightness(lightness + 1)
     }
   }
-
+  // const randomColor = eventData => {
+  //   if (eventData) {
+  //     setHue(Math.floor(Math.random() * 360))
+  //     setLightness(Math.floor(Math.random() * 100))
+  //     setSaturation(Math.floor(Math.random() * 100))
+  //   }
+  // }
   const divStyle = {
-    backgroundColor: `hsl(${hue}, ${saturation}, ${lightness} )`,
+    backgroundColor: `hsl(${hue}, ${saturation}%, ${lightness}% )`,
   }
   return (
     <>
@@ -31,17 +38,33 @@ const App = () => {
 
       <section class="colorSliderWrap">
         <span>Hue</span>
-        <input type="range" min={hue} max="360" onChange={changeHue} />
+        <input
+          type="range"
+          defaultValue={hue}
+          onChange={eventData => changeHue(eventData.target.value)}
+        />
         <span>Saturation</span>
-        <input type="range" min={hue} max="100" onChange={changeSaturation} />
+        <input
+          type="range"
+          defaultValue={saturation}
+          onChange={eventData => changeSaturation(eventData.target.value)}
+        />
         <span>Lightness</span>
         <input
           type="range"
-          min={lightness}
-          max="100"
-          onChange={changeLightness}
+          defaultValue={lightness}
+          onChange={eventData => changeLightness(eventData.target.value)}
         />
       </section>
+      <section>
+        <input type="text" {...hue} />
+        <input type="text" {...saturation} />
+        <input type="text" {...lightness} />
+      </section>
+
+      {/* <button onClick={eventData => randomColor(event.target.value)}>
+        Change Color
+      </button> */}
     </>
   )
 }
