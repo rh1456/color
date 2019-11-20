e966b3c33dadade1c15255e643d8fc52d48433f7
Reid Hanson <reidhans@buffalo.edu>
2019-11-19 19:34:08 -0500

    main requirements done, now adding stylings

--- src/App.js
+++ src/App.js
@@ -1,68 +1,13 @@
 import React, { useState } from 'react'
-// import Format from './components/setup'
+import Setup from './components/setup'
 
 const App = () => {
-  const [hue, setHue] = useState(360)
-  const [saturation, setSaturation] = useState(3)
-  const [lightness, setLightness] = useState(5)
-
-  const changeHue = eventData => {
-    if (eventData) {
-      setHue(hue + 1)
-    }
-  }
-
-  const changeSaturation = eventData => {
-    if (eventData) {
-      setSaturation(saturation + 1)
-    }
-  }
-  const changeLightness = eventData => {
-    if (eventData) {
-      setLightness(lightness + 1)
-    }
-  }
-  const randomColor = eventData => {
-    if (eventData) {
-      setHue(Math.floor(Math.random() * 360))
-      setLightness(Math.floor(Math.random() * 100))
-      setSaturation(Math.floor(Math.random() * 100))
-    }
-  }
-  const divStyle = {
-    backgroundColor: `hsl(${hue}, ${saturation}%, ${lightness}% )`,
-  }
   return (
     <>
-      <div style={divStyle} class="colorBox"></div>
-
-      <section class="colorSliderWrap">
-        <span>Hue</span>
-        <input
-          type="range"
-          defaultValue={hue}
-          onChange={eventData => changeHue(eventData.target.value)}
-        />
-        <span>Saturation</span>
-        <input
-          type="range"
-          defaultValue={saturation}
-          onChange={eventData => changeSaturation(eventData.target.value)}
-        />
-        <span>Lightness</span>
-        <input
-          type="range"
-          defaultValue={lightness}
-          onChange={eventData => changeLightness(eventData.target.value)}
-        />
-      </section>
-      <section>
-        <input type="text" {...hue} />
-        <input type="text" {...saturation} />
-        <input type="text" {...lightness} />
-      </section>
-
-      <button onClick={randomColor}>Change Color</button>
+      <h1>Color Changer with React</h1>
+      <main>
+        <Setup />
+      </main>
     </>
   )
 }
--- src/components/setup.js
+++ src/components/setup.js
@@ -1,1 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const Setup = () => {
+  const [hue, setHue] = useState(360)
+  const [saturation, setSaturation] = useState(3)
+  const [lightness, setLightness] = useState(5)
+
+  const changeHue = eventData => {
+    if (eventData) {
+      setHue(hue + 1)
+    }
+  }
+
+  const changeSaturation = eventData => {
+    if (eventData) {
+      setSaturation(saturation + 1)
+    }
+  }
+  const changeLightness = eventData => {
+    if (eventData) {
+      setLightness(lightness + 1)
+    }
+  }
+  const randomColor = eventData => {
+    if (eventData) {
+      setHue(Math.floor(Math.random() * 360))
+      setLightness(Math.floor(Math.random() * 100))
+      setSaturation(Math.floor(Math.random() * 100))
+    }
+  }
+  const divStyle = {
+    backgroundColor: `hsl(${hue}, ${saturation}%, ${lightness}% )`,
+  }
+  return (
+    <>
+      <div style={divStyle} class="colorBox"></div>
+
+      <section class="colorSliderWrap">
+        <span>Hue</span>
+        <input
+          type="range"
+          defaultValue={hue}
+          onChange={eventData => changeHue(eventData.target.value)}
+        />
+        <span>Saturation</span>
+        <input
+          type="range"
+          defaultValue={saturation}
+          onChange={eventData => changeSaturation(eventData.target.value)}
+        />
+        <span>Lightness</span>
+        <input
+          type="range"
+          defaultValue={lightness}
+          onChange={eventData => changeLightness(eventData.target.value)}
+        />
+      </section>
+      <section class="colorInfo">
+        <ul>
+          <li>{hue}</li>
+          <li>{saturation}</li>
+          <li>{lightness}</li>
+        </ul>
+      </section>
+
+      <button onClick={randomColor}>Change Color</button>
+    </>
+  )
+}
+export default Setup
